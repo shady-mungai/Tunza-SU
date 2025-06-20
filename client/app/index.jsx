bb49ddd8ca19b977c1bd4a8f0d5ea64957c503d2
shady-mungai <shadrackmungai10@gmail.com>
2025-06-20 17:28:25 +0300

    Configured tailwindcss for the application

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,14 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import "../global.css"
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
