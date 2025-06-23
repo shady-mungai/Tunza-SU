fe863ac7bd230d189ddca0cf551e57c9ca267c84
shady-mungai <shadrackmungai10@gmail.com>
2025-06-23 12:04:25 +0300

    Initialized student dashboard

---
+++ client/app/StudentDash.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StudentDash = () => {
+  return (
+    <View>
+      <Text>StudentDash</Text>
+    </View>
+  )
+}
+
+export default StudentDash
+
+const styles = StyleSheet.create({})
