4e7d50c2de5e33557dd5dd1b6c995787dbd33405
shady-mungai <shadrackmungai10@gmail.com>
2025-06-23 10:32:41 +0300

    configure the email regex to check for a .com

--- client/src/utils/validation.js
+++ client/src/utils/validation.js
@@ -1,6 +1,5 @@
 export const validateEmail = (email) => {
-  const emailRegex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/
   return {
     isValid: emailRegex.test(email),
     message: emailRegex.test(email) ? "" : "Please enter a valid email address",
